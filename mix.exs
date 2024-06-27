f01efc8261233bfe3d8c5afb0b8fc08bcbd21873
morten.lund@maskon.no <morten.lund@maskon.no>
2024-06-27 15:40:43 +0200

    retry

--- mix.exs
+++ mix.exs
@@ -49,7 +49,6 @@ defmodule Circuits.UART.MixProject do
         "src/*.[ch]",
         "src/ei_copy/*.[ch]",
         "src/Makefile",
-        "src/Makefile.win",
         "test",
         "mix.exs",
         "README.md",
