7ff434cd0e5c0f19148a458ad635964c27aff493
morten.lund@maskon.no <morten.lund@maskon.no>
2024-06-28 09:08:58 +0200

    Add checksum

--- checksum.exs
+++ checksum.exs
@@ -1,2 +1,4 @@
 %{
+  "circuits_uart-nif-2.16-x86_64-linux-gnu-1.5.3.tar.gz" => "sha256:7989bb311baa38ef545250282aa065d23281c46dfb8faabe4c653487bdbded5c",
+  "circuits_uart-nif-2.16-x86_64-w64-mingw32-gcc-1.5.3.tar.gz" => "sha256:b48e9c608b3b099351d10aa3c8c94fcaa5ea7b07909a68cdbaefa6c14a5b05e1",
 }
