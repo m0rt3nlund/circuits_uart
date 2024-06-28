1b56c176ef45db2978dd10d74116a771bc41046e
morten.lund@maskon.no <morten.lund@maskon.no>
2024-06-28 09:45:28 +0200

    add checksum

--- checksum.exs
+++ checksum.exs
@@ -1,4 +1,3 @@
 %{
-  "circuits_uart-nif-2.17-x86_64-windows-gcc-1.5.3.tar.gz" => "sha256:b48e9c608b3b099351d10aa3c8c94fcaa5ea7b07909a68cdbaefa6c14a5b05e1",
-  "circuits_uart-nif-2.17-x86_64-windows-msvc-1.5.3.tar.gz" => "sha256:b48e9c608b3b099351d10aa3c8c94fcaa5ea7b07909a68cdbaefa6c14a5b05e1",
+  "circuits_uart-nif-2.16-x86_64-windows-msvc-1.5.3.tar.gz" => "sha256:b48e9c608b3b099351d10aa3c8c94fcaa5ea7b07909a68cdbaefa6c14a5b05e1",
 }
